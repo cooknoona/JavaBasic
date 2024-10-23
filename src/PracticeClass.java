04c3a6b1821968031d71e894becd5acce848e07f
Cook <cook961004@gmail.com>
2024-10-23 15:08:42 +0900

    JDBC connecting

---
+++ src/PracticeClass.java
@@ -0,0 +1,6 @@
+public class PracticeClass {
+    public static void main(String[] args) {
+        System.out.println("\n=====글 작성 메뉴=====");
+        System.out.println("=====카테고리 선택하세요=====");
+    }
+}
